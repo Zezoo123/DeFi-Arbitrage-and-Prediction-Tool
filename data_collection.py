8f9a67e3be17079a67e721190675cbbcbd729da8
Zeyad Abdellatif Awadalla <zeyad.abdellatifawadalla@student.manchester.ac.uk>
2024-10-08 16:32:28 +0100

    Implement function fetch_prices_over_time

--- data_collection.py
+++ data_collection.py
@@ -1,9 +1,12 @@
 import requests
 import pandas as pd
 
-url_ping = 'https://api.coingecko.com/api/v3/ping'
-url_price = 'https://api.coingecko.com/api/v3/simple/price'
+url_api = 'https://api.coingecko.com/api/v3'
+url_ping = url_api + '/ping'
+url_price = url_api + '/simple/price'
+url_coins = url_api + '/coins'
 
+DAYS = 30
 crypto_ids = ['bitcoin', 'ethereum', 'litecoin', 'ripple', 'cardano']
 
 # API key
@@ -28,13 +31,35 @@ def fetch_current_prices(url, headers, crypto_ids):
 
     return data
 
-prices = fetch_current_prices(url_price, headers, crypto_ids)
-current_prices_fh = 'current_prices.csv'
 
-if prices:
-    df = pd.DataFrame(prices).T.reset_index() # Transpose to get currencies as rows
-    df.columns = ['crypto', 'price']
-    df.to_csv(current_prices_fh, index=False)
-    print("Prices saved succesfully to " + current_prices_fh)
-else:
-    print("Failed to save prices")
+"""
+Fetch the prices of a specific cryptocurrency over a specified number of days
+"""
+def fetch_prices_over_time(url, crypto_id, days=DAYS):
+    url = url + f'/{crypto_id}/market_chart'
+    params = {
+        'vs_currencies': 'USD',
+        'days': days
+    }
+    response = requests.get(url, params = params)
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print('Failed to retrieve data from the API')
+        print(f'Error Code: {response.status_code}')
+        return None
+
+    return data
+
+# current_prices = fetch_current_prices(url_price, headers, crypto_ids)
+# current_prices_fh = 'current_prices.csv'
+
+# if current_prices:
+#     df = pd.DataFrame(prices).T.reset_index() # Transpose to get currencies as rows
+#     df.columns = ['crypto', 'price']
+#     df.to_csv(current_prices_fh, index=False)
+#     print("Prices saved succesfully to " + current_prices_fh)
+# else:
+#     print("Failed to save prices")
+
