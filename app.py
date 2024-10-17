4e9c107235f2aac2e250fe6c3002fd914099d0ed
Zeyad Abdellatif Awadalla <zeyad.abdellatifawadalla@student.manchester.ac.uk>
2024-10-17 19:34:24 +0100

    Plot kLine chart on website

--- app.py
+++ app.py
@@ -8,13 +8,18 @@ from plot import plot_prices_over_time, plot_klines
 
 app = Flask(__name__)
 
+
+
 # Route for the main page
 @app.route('/')
 def index():
+    intervals = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d']
+    crypto_ids = ['BTC', 'ETH', 'BNB', 'XRP', 'LTC', 'ADA']
+
     if time.time() - price_cache.get('last_refresh', 0) > 300:
         load_current_prices()
     current_prices = pd.read_csv('data/coingecko/current_prices.csv')
-    return render_template('index.html', prices=current_prices)
+    return render_template('index.html', prices=current_prices, intervals=intervals, crypto_ids=crypto_ids)
 
 @app.route('/historical/<crypto_id>')
 def crypto_page(crypto_id):
@@ -32,6 +37,15 @@ def crypto_page(crypto_id):
 
     return render_template('crypto_page.html', current_price=current_prices.iloc[0]['price'], historical_data=historical_data, crypto_id=crypto_id, graph_url=graph_url)
 
+@app.route('/kLine/chart', methods=['GET'])
+def kline_chart():
+    crypto_id = request.args.get('crypto_id')
+    interval = request.args.get('interval')
+
+    kline_data = pd.read_csv(f'data/binance/{crypto_id}/{interval}.csv')
+    kline_html = plot_klines(kline_data, interval, crypto_id)
+
+    return render_template('kline_chart.html', kline_html=kline_html, interval=interval, crypto_id=crypto_id)
 
 """
 Route for the reload prices button.
@@ -44,6 +58,14 @@ def reload_prices_func():
 def refresh_prices():
     coingecko_data_collection.save_current_prices()
     load_current_prices()
+ 
+
+@app.route('/api/get_data', methods=['POST'])
+def get_data():
+    selected_crypto = request.form['crypto']
+    selected_interval = request.form['interval']
+
+    return redirect(url_for('kline_chart', crypto_id=selected_crypto, interval=selected_interval))
 
 """
 Cache to hold prices in memory for efficiency
