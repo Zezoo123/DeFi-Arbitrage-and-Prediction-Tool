881814ec8e9396933e6c5a208ff127e912307286
Zeyad Abdellatif Awadalla <zeyad.abdellatifawadalla@student.manchester.ac.uk>
2024-11-08 19:51:23 +0000

    Load uniswap abi json

--- arbitrage/smart_contract.py
+++ arbitrage/smart_contract.py
@@ -1,6 +1,7 @@
-from web3 import Web3
+from web3 import Web3, eth
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 
@@ -18,3 +19,15 @@ def check_connection(web3):
         print("Could not connect to the Ethereum network")
     
 check_connection(w3)
+
+def load_abi_json(filename):
+    try:
+        with open(filename) as f:
+            abi = json.load(f)
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+        return None
+
+uniswap_router_abi_json_fh = "uniswap_v2_router_abi.json"
+uniswap_router_abi = load_abi_json(uniswap_router_abi_json_fh)
+uniswap_router_address = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
