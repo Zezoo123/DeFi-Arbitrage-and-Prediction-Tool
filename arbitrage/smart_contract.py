4adb42311dba481dd9cd9b5a2193b4a2da4172d2
Zeyad Abdellatif Awadalla <zeyad.abdellatifawadalla@student.manchester.ac.uk>
2024-11-08 20:31:44 +0000

    Use WETH instead of ETH address as intermediary #8

--- arbitrage/smart_contract.py
+++ arbitrage/smart_contract.py
@@ -25,7 +25,6 @@ def load_abi_json(filename):
     try:
         with open(filename) as f:
             abi = json.load(f)
-        f.close()    
         return abi
     except FileNotFoundError:
         print(f"File '{filename}' not found.")
@@ -33,21 +32,27 @@ def load_abi_json(filename):
 
 uniswap_router_abi_json_fh = "uniswap_v2_router_abi.json"
 uniswap_router_abi = load_abi_json(uniswap_router_abi_json_fh)
-uniswap_router_address = w3.to_checksum_address('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
+if uniswap_router_abi:
+    uniswap_router_address = w3.to_checksum_address('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
 
-# Create contract instance
-uniswap_router_contract = w3.eth.contract(address=uniswap_router_address, abi=uniswap_router_abi)   
+    # Create contract instance
+    uniswap_router_contract = w3.eth.contract(address=uniswap_router_address, abi=uniswap_router_abi)   
 
 def get_uniswap_price(token_address):
-    global w3
     eth_address = w3.to_checksum_address('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE')
+    weth_address = w3.to_checksum_address('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')  # WETH address
     token_address = w3.to_checksum_address(token_address)
     amount_in = w3.to_wei(1, 'ether')
+
+    path = [weth_address, token_address]
     try:
         print(f"Fetching price for token: {token_address}")
-        print(f"Using path: [{eth_address}, {token_address}]")
-        amounts_out = uniswap_router_contract.functions.getAmountsOut(amount_in, [eth_address, token_address]).call()
+        print(f"Using path: {path}")
+        amounts_out = uniswap_router_contract.functions.getAmountsOut(amount_in, path).call()
         return w3.fromWei(amounts_out[1], 'ether')
+    except ValueError as e:
+        print(f"Error fetching price - likely due to contract call: {e}")
+        return None
     except Exception as e:
         print(f"Error fetching price: {e}")
         return None
