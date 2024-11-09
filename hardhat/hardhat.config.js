e713426ef7c873862c811b237936bb121d326db1
Zeyad Abdellatif Awadalla <zeyad.abdellatifawadalla@student.manchester.ac.uk>
2024-11-09 20:09:08 +0000

    I am burnt out and its not working #8

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -7,8 +7,8 @@ module.exports = {
   solidity: "0.8.27",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}",
-      accounts: ['${process.env.METAMASK_API_KEY}']
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`${process.env.METAMASK_PRIVATE_KEY}`]
     }
   }
 };
