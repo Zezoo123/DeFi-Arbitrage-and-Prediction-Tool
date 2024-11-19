f7668ddc5b427c956d6b7d489a1b4c0896eafb70
Zeyad Abdellatif Awadalla <zeyad.abdellatifawadalla@student.manchester.ac.uk>
2024-11-19 18:22:06 +0000

    Pass arbitrage test

--- hardhat/test/Arbitrage.ts
+++ hardhat/test/Arbitrage.ts
@@ -24,16 +24,16 @@ describe("Arbitrage Testing", function() {
 
         // Deploy Router 1 with exchange rate of 2
         const RouterFactory = await ethers.getContractFactory("MockUniswapRouter");
-        router1 = await RouterFactory.connect(owner).deploy(mockWETH.address, mockDAI.address, 2);
+        router1 = await RouterFactory.connect(owner).deploy(await mockWETH.getAddress(), await mockDAI.getAddress(), 2);
         await router1.waitForDeployment();
 
         // Deploy Router 2 with exchange rate of 1
-        router2 = await RouterFactory.connect(owner).deploy(mockWETH.address, mockDAI.address, 1);
+        router2 = await RouterFactory.connect(owner).deploy(await mockWETH.getAddress(), await mockDAI.getAddress(), 1);
         await router2.waitForDeployment();
 
         // Ensure addresses are not null
-        expect(mockWETH.address).to.not.be.null;
-        expect(mockDAI.address).to.not.be.null;
+        expect(await mockWETH.getAddress()).to.not.be.null;
+        expect(await mockDAI.getAddress()).to.not.be.null;
         expect(await router1.getAddress()).to.not.be.null;
         expect(await router2.getAddress()).to.not.be.null;
       
@@ -50,38 +50,41 @@ describe("Arbitrage Testing", function() {
 
     it("should perform arbitrage between 2 routers", async function() {
         // Transfer WETH to arbitrageur
-        await mockWETH.connect(owner).transfer(arbitrageur.address, ethers.parseEther("100"));
+        await mockWETH.connect(owner).transfer(await arbitrageur.getAddress(), ethers.parseEther("100"));
 
         // Approve routers to spend arbitrageur's WETH
-        await mockWETH.connect(arbitrageur).approve(router1.address, ethers.parseEther("100"));
-        await mockWETH.connect(arbitrageur).approve(router2.address, ethers.parseEther("100"));
+        const router1Address = await router1.getAddress();
+        const router2Address = await router2.getAddress();
+
+        await mockWETH.connect(arbitrageur).approve(router1Address, ethers.parseEther("100"));
+        await mockWETH.connect(arbitrageur).approve(router2Address, ethers.parseEther("100"));
 
         // Perform the first swap on router1 (WETH -> DAI)
         await router1.connect(arbitrageur).swapExactTokensForTokens(
             ethers.parseEther("50"), // amountIn
             ethers.parseEther("50"), // amountOutMin (set low to ensure swap)
-            [mockWETH.address, mockDAI.address],
-            arbitrageur.address,
+            [await mockWETH.getAddress(), await mockDAI.getAddress()],
+            await arbitrageur.getAddress(),
             BigInt(Math.floor(Date.now() / 1000)) + 60n * 10n // deadline
         );
 
         // Check DAI balance after first swap
-        const daiBalanceAfterSwap1 = await mockDAI.balanceOf(arbitrageur.address);
+        const daiBalanceAfterSwap1 = await mockDAI.balanceOf(await arbitrageur.getAddress());
         expect(daiBalanceAfterSwap1).to.be.greaterThan(ethers.parseEther("0"));
 
         // Approve router2 to spend arbitrageur's DAI
-        await mockDAI.connect(arbitrageur).approve(router2.address, daiBalanceAfterSwap1);
+        await mockDAI.connect(arbitrageur).approve(router2Address, daiBalanceAfterSwap1);
 
         // Perform the second swap on router2 (DAI -> WETH)
         await router2.connect(arbitrageur).swapExactTokensForTokens(
             daiBalanceAfterSwap1,
             ethers.parseEther("25"), // amountOutMin (set low to ensure swap)
-            [mockDAI.address, mockWETH.address],
-            arbitrageur.address,
+            [await mockDAI.getAddress(), await mockWETH.getAddress()],
+            await arbitrageur.getAddress(),
             BigInt(Math.floor(Date.now() / 1000)) + 60n * 10n // deadline
         );
 
-        const wethBalanceAfterSwap2 = (await mockWETH.balanceOf(arbitrageur.address)).toBigInt();
+        const wethBalanceAfterSwap2 = (await mockWETH.balanceOf(await arbitrageur.getAddress()));
         const initialWethBalance = ethers.parseEther("50");
         const profit = wethBalanceAfterSwap2 - initialWethBalance;
         
